dc87239a8756dc8cb8d174aef683902defafef5c
JesseNReynolds <jessenreynolds@gmail.com>
2020-12-15 11:26:00 -0700

    migration for bands

--- db/migrate/20201215180639_create_bands.rb
+++ db/migrate/20201215180639_create_bands.rb
@@ -1,6 +1,9 @@
 class CreateBands < ActiveRecord::Migration[6.0]
   def change
     create_table :bands do |t|
+      t.integer :leader_id
+      t.text :bio
+      t.string :name
 
       t.timestamps
     end
