9760ae51ca883ea722fa132e0db30544178ed1ef
JesseNReynolds <jessenreynolds@gmail.com>
2020-12-15 11:30:50 -0700

    migration for gigs

--- db/migrate/20201215180829_create_gigs.rb
+++ db/migrate/20201215180829_create_gigs.rb
@@ -1,6 +1,12 @@
 class CreateGigs < ActiveRecord::Migration[6.0]
   def change
     create_table :gigs do |t|
+      t.integer :band_id
+      t.integer :venue_id
+      t.string :approval
+      t.string :venue_message
+      t.datetime  :time
+      t.string :duration
 
       t.timestamps
     end
