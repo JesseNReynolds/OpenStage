8ab1cbfd10cc7fc4b7fda92e7a8f91d913f37e20
JesseNReynolds <jessenreynolds@gmail.com>
2020-12-15 13:21:21 -0700

    added some user vaildations, password confirmation not working.

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,8 @@ class User < ApplicationRecord
     has_many :bands, through: :band_members
     has_many :gigs, through: :bands
 
+    validates :name, presence: true 
+    validates :email, presence: true 
+    validates :email, uniqueness: true
+
 end
