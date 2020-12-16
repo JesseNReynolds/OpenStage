15298543adb7ad0f33422ebf1ca0d9bf9e23cd2c
JesseNReynolds <jessenreynolds@gmail.com>
2020-12-16 13:43:55 -0700

    Added city and state columns to venues

---
+++ db/migrate/20201216204021_add_city_and_state_to_venue.rb
@@ -0,0 +1,8 @@
+class AddCityAndStateToVenue < ActiveRecord::Migration[6.0]
+
+  def change
+    add_column :venues, :city, :string
+    add_column :venues, :state, :string
+  end
+  
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_15_204550) do
+ActiveRecord::Schema.define(version: 2020_12_16_204021) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -59,6 +59,8 @@ ActiveRecord::Schema.define(version: 2020_12_15_204550) do
     t.text "description"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "city"
+    t.string "state"
   end
 
 end
