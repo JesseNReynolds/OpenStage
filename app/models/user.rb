f3c4ba2a848cdba712d740a981f16be07062b7af
JesseNReynolds <jessenreynolds@gmail.com>
2020-12-17 13:26:27 -0700

    Leader has many bands as band leader

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User < ApplicationRecord
 
     has_secure_password
     
+    has_many :lead_bands, class_name: "Band", foreign_key: :leader_id
     has_many :band_members
     has_many :bands, through: :band_members
     has_many :gigs, through: :bands
