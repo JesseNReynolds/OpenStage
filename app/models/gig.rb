34a51674c965bf99795ed7aa83f6f21703eb7aa6
JesseNReynolds <jessenreynolds@gmail.com>
2020-12-17 12:23:22 -0700

    Validations for gig creation

--- app/models/gig.rb
+++ app/models/gig.rb
@@ -4,5 +4,8 @@ class Gig < ApplicationRecord
     belongs_to :venue
     belongs_to :band, optional: true
     
+    validates :duration, numericality: { greater_than_or_equal_to: 30, less_than_or_equal_to: 180 }
+    validates :duration, presence: true
+
     
 end
