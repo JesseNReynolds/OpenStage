07c997cb09b8eeaadef1e666a86cd1741a887ae6
JesseNReynolds <jessenreynolds@gmail.com>
2020-12-18 15:38:53 -0700

    Helper method to assist with band-leader contextuals in views.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
-    helper_method :venue_is_logged_in?, :current_venue, :current_venue_id, :user_is_logged_in?, :current_user_id, :current_user
+    helper_method :venue_is_logged_in?, :current_venue, :current_venue_id,
+     :user_is_logged_in?, :current_user_id, :current_user, :current_user_is_leader?
 
     def home
         render '/home'
@@ -23,12 +24,16 @@ class ApplicationController < ActionController::Base
         !!session[:user_id]
     end
 
-    def self.current_user_id(session)
+    def current_user_id
         session[:user_id]
     end
 
-    def self.current_user(session)
+    def current_user
         User.find(session[:user_id])
     end
 
+    def current_user_is_leader?
+        !current_user.lead_bands.empty?
+    end
+        
 end
